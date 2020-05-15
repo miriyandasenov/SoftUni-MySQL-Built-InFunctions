29af818445946d6a8a15dd372c6740f541a649a1
miriyandasenov <63752573+miriyandasenov@users.noreply.github.com>
2020-05-15 22:30:14 +0300

    Mix of Peak and River Names

---
+++ Exercise11.sql
@@ -0,0 +1,5 @@
+SELECT p.peak_name, r.river_name,
+LOWER(CONCAT(p.peak_name,SUBSTRING(r.river_name,2)))  AS 'mix'
+FROM peaks AS p, rivers AS r
+WHERE RIGHT(p.peak_name,1) = LEFT(r.river_name,1)
+ORDER BY `mix`;
